0bb8e356eb891f3c8ad6d601541f9a0b93ec0eab
Trang Tran <tran.thithuytrang.da@gmail.com>
2026-04-20 09:59:40 +0200

    define user role

--- init-mongo.js
+++ init-mongo.js
@@ -1,9 +1,17 @@
-db = db.getSiblingDB("DBMedical");
+db = db.getSiblingDB('DBMedical');
 
 db.createUser({
-  user: "app_user",
-  pwd: "securepassword",
-  roles: [
-    { role: "readWrite", db: "DBMedical" }
-  ]
+    user: "app_user",
+    pwd: "app_password123",
+    roles: [
+        { role: "readWrite", db: "DBMedical" },
+        { role: "dbAdmin", db: "DBMedical" }
+    ]
 });
+
+db.createCollection("Healthcare");
+db.Healthcare.createIndex({ "Name": 1 });
+db.Healthcare.createIndex({ "Doctor": 1 });
+db.Healthcare.createIndex({ "Hospital": 1 });
+
+print("MongoDB initialization complete: admin and app_user created");
